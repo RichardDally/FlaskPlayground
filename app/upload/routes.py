b0e8150b856919ba8099e0e651604ba05188d4f8
Richard Dally <r.dally@protonmail.com>
2023-07-26 08:45:50 +0200

    Stop using hardcoded directory name, use config instead

--- app/upload/routes.py
+++ app/upload/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request
+from pathlib import Path
+from flask import render_template, request, current_app
 from app.upload import bp
 
 
@@ -10,7 +11,7 @@ def index():
 
         video_file = request.files['video']
         # TODO: add file type checking before saving
-        video_file.save('uploads/' + video_file.filename)
+        video_file.save(str(Path(current_app.config['UPLOAD_FOLDER'], video_file.filename)))
         return render_template('upload_success.html', video_file=video_file)
 
     return render_template('upload.html')
